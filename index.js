283184be69a52c132ff391f2a88ae07c813256c2
indexduo <83288588+IndexDuo@users.noreply.github.com>
2025-02-05 07:31:30 -0500

    complete assignment

---
+++ index.js
@@ -0,0 +1,41 @@
+// Your code here
+
+// True/False questions are from https://www.classpoint.io/blog/true-or-false-questions-for-kids
+
+var questionsArr = [
+    {
+        question: "The ocean is salty.",
+        answer: true,
+    },
+    {
+        question: "Penguins can fly.",
+        answer: false,
+    },
+    {
+        question: "The Earth is round.",
+        answer: true,
+    },
+    {
+        question: "Seeds grow into plants",
+        answer: true,
+    },
+    {
+        question: "All insects have six legs.",
+        answer: true,
+    },
+];
+
+function runQuiz() {
+    // alert(questionArr.length);
+    var score = 0;
+    for (let i = 0; i < questionsArr.length; i++) {
+        // let userAnswer = window.confirm(questionArr[i].question);
+        if (
+            window.confirm(questionsArr[i].question) == questionsArr[i].answer
+        ) {
+            score += 1;
+        }
+    }
+    score = Math.round((score / questionsArr.length) * 100);
+    window.alert("Your score is " + score + "%");
+}
